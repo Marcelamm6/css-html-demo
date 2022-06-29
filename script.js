4ba00d5ffa7e2b5ab2bf3f4ced6db5edeb4fd273
Marcela <marcelamm6@gmail.com>
2022-06-29 10:56:16 -0300

    responsive-2-incompleto

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-"use strict";
+'use strict';
 
 // ---------smooth scrolling---------
 
-const btnInformacoes = document.querySelector(".btn-menu");
-const section1 = document.querySelector("#section1");
-const btnHeart = document.querySelector(".heart");
-const section2 = document.querySelector("#section2");
-const btnPicture = document.querySelector(".picture");
-const section3 = document.querySelector("#section3");
-const btnMoon = document.querySelector(".moon");
-const section4 = document.querySelector("#section4");
+const btnInformacoes = document.querySelector('.btn-menu');
+const section1 = document.querySelector('#section1');
+const btnHeart = document.querySelector('.heart');
+const section2 = document.querySelector('#section2');
+const btnPicture = document.querySelector('.picture');
+const section3 = document.querySelector('#section3');
+const btnMoon = document.querySelector('.moon');
+const section4 = document.querySelector('#section4');
 
 function smooth(btn, sectionS) {
-  btn.addEventListener("click", function (e) {
+  btn.addEventListener('click', function (e) {
     e.preventDefault();
 
-    sectionS.scrollIntoView({ behavior: "smooth" });
+    sectionS.scrollIntoView({ behavior: 'smooth' });
   });
 }
 
@@ -26,47 +26,45 @@ smooth(btnMoon, section4);
 
 // -----------Tabbed component-----------
 
-const tabComponent = document.querySelector(".paginamenu");
+const tabComponent = document.querySelector('.paginamenu');
 
-const tabBotoes = document.querySelector(".tab");
-const botoes = document.querySelectorAll(".btnp2");
+const tabBotoes = document.querySelector('.tab');
+const botoes = document.querySelectorAll('.btnp2');
 
-tabBotoes.addEventListener("click", function (e) {
-  const clicked = e.target.closest(".btnp2");
+tabBotoes.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.btnp2');
   //   console.log(clicked);
 
   //Guard clause -- finish the function when there's "nothing clicked" (se clicar entre os botões não vai dar erro)
   if (!clicked) return;
 
   //Active tab
-  botoes.forEach((t) => t.classList.remove("btnactive"));
-  clicked.classList.add("btnactive");
+  botoes.forEach(t => t.classList.remove('btnactive'));
+  clicked.classList.add('btnactive');
 
   //Activate content
 
-  const conteudos = document.querySelectorAll(".conteudot");
+  const conteudos = document.querySelectorAll('.conteudot');
 
-  conteudos.forEach((c) => c.classList.remove("conteudo-active"));
+  conteudos.forEach(c => c.classList.remove('conteudo-active'));
 
   //   console.log(clicked.dataset.tab);
 
   document
     .querySelector(`.click${clicked.dataset.tab}`)
-    .classList.add("conteudo-active");
+    .classList.add('conteudo-active');
 
-  let pqp = document.querySelector(
-    `.maisquetextochato--${clicked.dataset.tab}`
-  );
+  let texto = document.querySelector(`.text--${clicked.dataset.tab}`);
 
-  //   console.log(pqp);
+  // console.log(texto);
 });
 
 // ----------------Card change--------------
 
-const gostamos = document.querySelector(".gostamos");
+const gostamos = document.querySelector('.gostamos');
 
-gostamos.addEventListener("click", function (e) {
-  const clic = e.target.closest(".imgdq");
+gostamos.addEventListener('click', function (e) {
+  const clic = e.target.closest('.imgdq');
 
   //   console.log(clic);
 
@@ -79,7 +77,7 @@ gostamos.addEventListener("click", function (e) {
 
   if (picFotinha) {
     picFotinha.src = picFotinha.src.replace(
-      "images/cartavirada.png",
+      'images/cartavirada.png',
       `images/${clic.dataset.pic}.jpg`
     );
   }
@@ -87,10 +85,10 @@ gostamos.addEventListener("click", function (e) {
 
 // ---------------Carousel------------------
 
-const carousel = document.querySelector(".fotos-carousel");
-const sidePic = document.querySelectorAll(".sides");
-const btnLeft = document.querySelector(".left");
-const btnRight = document.querySelector(".right");
+const carousel = document.querySelector('.fotos-carousel');
+const sidePic = document.querySelectorAll('.sides');
+const btnLeft = document.querySelector('.left');
+const btnRight = document.querySelector('.right');
 
 let curPicture = 0;
 const maxPictures = sidePic.length;
@@ -124,12 +122,12 @@ const prevPicture = function () {
   andarCarousel(curPicture);
 };
 
-btnRight.addEventListener("click", nextPicture);
-btnLeft.addEventListener("click", prevPicture);
+btnRight.addEventListener('click', nextPicture);
+btnLeft.addEventListener('click', prevPicture);
 
 //Usar teclas pra mover as fotos
-document.addEventListener("keydown", function (e) {
+document.addEventListener('keydown', function (e) {
   // console.log(e);
-  if (e.key === "ArrowLeft") prevPicture();
-  if (e.key === "ArrowRight") nextPicture();
+  if (e.key === 'ArrowLeft') prevPicture();
+  if (e.key === 'ArrowRight') nextPicture();
 });
